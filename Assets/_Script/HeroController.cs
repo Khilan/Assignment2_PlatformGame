d4bc78166541ec54bf1aab48709304cd6533cd57
Khilan <khilan9696@gmail.com>
2016-02-27 20:17:05 -0500

    Camera is moving with Hero

--- Assets/_Script/HeroController.cs
+++ Assets/_Script/HeroController.cs
@@ -23,8 +23,8 @@ public class HeroController : MonoBehaviour {
 	public VelocityRange velocityRange;
 	public float moveForce;
 	public float jumpForce;
-//	public Transform groundCheck;
-//	public Transform camera;
+	public Transform groundCheck;
+	public Transform camera;
 //	public GameController gameController;
 
 	// PRIVATE  INSTANCE VARIABLES
@@ -41,9 +41,9 @@ public class HeroController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		//Intialize public Instance Variable
-		this.velocityRange = new VelocityRange(300f,1000f);
-		this.moveForce = 50f;
-		this.jumpForce = 500f;
+		this.velocityRange = new VelocityRange(300f,30000f);
+	//	this.moveForce = 1000f;
+	//	this.jumpForce = 35000f;
 
 
 
@@ -63,6 +63,16 @@ public class HeroController : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 
+		Vector3 currentPosition = new Vector3 (this._transform.position.x, this._transform.position.y, -10f);
+		this.camera.position = currentPosition;
+
+
+		this._isGrounded = Physics2D.Linecast (this._transform.position, 
+											   this.groundCheck.position,
+												1 << LayerMask.NameToLayer ("Ground"));
+		
+		Debug.DrawLine (this._transform.position, this.groundCheck.position);
+
 		float forceX = 0f;
 		float forceY = 0f;
 
@@ -70,31 +80,58 @@ public class HeroController : MonoBehaviour {
 		float absVelX = Mathf.Abs (this._rigidBody2D.velocity.x);
 		float absVelY = Mathf.Abs (this._rigidBody2D.velocity.y);
 
-		//Get a number between -1 to 1
-		this._move = Input.GetAxis ("Horizontal");
-		this._jump = Input.GetAxis ("Vertical");
 
-		if (this._move != 0) {
-			this._animator.SetInteger ("Anim_State", 1);
 
-			if (this._move > 0) {
-				this._facingRight = true;
-				this._flip ();
+
+		if (this._isGrounded) {
+			if (this._isGrounded) {
+				// gets a number between -1 to 1 for both Horizontal and Vertical Axes
+				this._move = Input.GetAxis ("Horizontal");
+				this._jump = Input.GetAxis ("Vertical");
+
+				if (this._move != 0) {
+					if (this._move > 0) {
+						// movement force
+						if (absVelX < this.velocityRange.maximum) {
+							forceX = this.moveForce;
+						}
+						this._facingRight = true;
+						this._flip ();
+					}
+					if (this._move < 0) {
+						// movement force
+						if (absVelX < this.velocityRange.maximum) {
+							forceX = -this.moveForce;
+						}
+						this._facingRight = false;
+						this._flip ();
+					}
+
+					// call the walk clip
+					this._animator.SetInteger ("Anim_State", 1);
+				} else {
+
+					// set default animation state to "idle"
+					this._animator.SetInteger ("Anim_State", 0);
+				}
+
+				if (this._jump > 0) {
+					// jump force
+					if (absVelY < this.velocityRange.maximum) {
+						//this._jumpSound.Play ();
+						forceY = this.jumpForce;
+					}
+
+				}
+			} else {
+				// call the "jump" clip
+				this._animator.SetInteger ("Anim_State", 2);
 			}
-			if (this._move < 0) {
-				this._facingRight = false;
-				this._flip ();
-			}
-		} else {
-			
-			//Set Default Animatation state to Idle
-			this._animator.SetInteger ("Anim_State", 0);
+
+			// Apply the forces to the player
+			this._rigidBody2D.AddForce (new Vector2 (forceX, forceY));
 		}
-			
-		if (this._jump > 0) {
-			//Jump Animation
-			this._animator.SetInteger ("Anim_State", 2);
-		}
+
 	}
 
 	//PRIVATE Methods
